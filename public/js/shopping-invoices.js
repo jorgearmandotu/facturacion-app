511f25e2aaa311705ef0738b3d24f5062b60c789
armando <jorgearmandou@gmail.com>
2023-02-17 12:45:11 -0500

    se edita vista de carge de facturas

--- public/js/shopping-invoices.js
+++ public/js/shopping-invoices.js
@@ -1,47 +1,69 @@
+let selectProducts = document.getElementById('selectProducts');
+let inputQuantity = document.getElementById('inputQuantity');
+let inputVlrUnit = document.getElementById('inputVlrUnitario');
+let inputVlrTotal = document.getElementById('inputVlrTotal');
+let formProducts = document.getElementById('formProducts');
+let itemsTotal = 0;
+let itemsView = 0;
 
-function addRow(){
+let formProductsList = new FormData();
+
+function changeVlrUnit(){
+    if(inputQuantity !== '' && inputVlrTotal !== ''){
+        inputVlrTotal.value = Number(inputQuantity.value)*inputVlrUnit.value;
+    }
+}
+
+function addRow(quantity, vlrUnit, VlrTotal, item, productText, itemsTotal){
     const rowForm = document.getElementById('rowForm');
     let row = document.createDocumentFragment();
     const divRow = document.createElement('div');
     divRow.setAttribute('class', 'form-row');
+    divRow.setAttribute('id', `row${item}`);
     const divNumber = document.createElement('div');
     divNumber.setAttribute('class', 'col-md-1 border border-dark p-0');
     const inputNumber = document.createElement('input');
     inputNumber.setAttribute('type', 'text');
-    inputNumber.setAttribute('value', '10');
-    inputNumber.setAttribute('class', 'form-control col-md-12');
+    inputNumber.setAttribute('value', itemsTotal);
+    inputNumber.setAttribute('id', `number${item}`);
+    inputNumber.setAttribute('class', 'form-control col-md-12 inputDisabled');
+    inputNumber.setAttribute('disabled', '');
     const divProduct = document.createElement('div');
-    divProduct.setAttribute('class', 'col-md-4 border border-dark p-0');
+    divProduct.setAttribute('class', 'col-md-5 border border-dark p-0');
     const inputProduct = document.createElement('input');
     inputProduct.setAttribute('type', 'text');
-    inputProduct.setAttribute('value', 'product');
-    inputProduct.setAttribute('class', 'form-control col-md-12');
+    inputProduct.setAttribute('value', productText);
+    inputProduct.setAttribute('class', 'form-control col-md-12 inputDisabled');
+    inputProduct.setAttribute('disabled', '');
     const divCant = document.createElement('div');
-    divCant.setAttribute('class', 'col-md-2 border border-dark p-0')
+    divCant.setAttribute('class', 'col-md-1 border border-dark p-0')
     const inputCant = document.createElement('input');
     inputCant.setAttribute('type', 'number');
-    inputCant.setAttribute('value', '20');
-    inputCant.setAttribute('class', 'form-control col-md-12');
+    inputCant.setAttribute('value', quantity);
+    inputCant.setAttribute('class', 'form-control col-md-12 inputDisabled');
+    inputCant.setAttribute('disabled', '');
     const divUnit = document.createElement('div');
     divUnit.setAttribute('class', 'col-md-2 border border-dark p-0');
     const inputUnit = document.createElement('input');
     inputUnit.setAttribute('type', 'text');
-    inputUnit.setAttribute('value', '0');
-    inputUnit.setAttribute('class', 'form-control col-md-12');
+    inputUnit.setAttribute('value', vlrUnit);
+    inputUnit.setAttribute('class', 'form-control col-md-12 inputDisabled');
+    inputUnit.setAttribute('disabled', '');
     const divTotal = document.createElement('div');
     divTotal.setAttribute('class', 'col-md-2 border border-dark p-0');
     const inputTotal = document.createElement('input');
     inputTotal.setAttribute('type', 'text');
-    inputTotal.setAttribute('value', '20000');
-    inputTotal.setAttribute('class', 'form-control col-md-12');
+    inputTotal.setAttribute('value', VlrTotal);
+    inputTotal.setAttribute('class', 'form-control col-md-12 inputDisabled');
+    inputTotal.setAttribute('disabled', '');
     const divOptions = document.createElement('div');
-    divOptions.setAttribute('class', 'col-md-1 border border-dark p-0')
+    divOptions.setAttribute('class', 'col-md-1  ')
     const buttonPlus = document.createElement('button');
-    buttonPlus.setAttribute('class', 'btn btn-success');
+    buttonPlus.setAttribute('class', 'btn btn-danger');
     buttonPlus.setAttribute('type', 'button');
-    buttonPlus.setAttribute('onclick', 'clickPlus(Event)');
+    buttonPlus.setAttribute('onclick', `rowRemove(${item})`);
     const icon = document.createElement('i');
-    icon.setAttribute('class', 'fas fa-solid fa-plus');
+    icon.setAttribute('class', 'far fa-trash-alt');
     buttonPlus.appendChild(icon);
     divOptions.append(buttonPlus);
 
@@ -62,14 +84,90 @@ function addRow(){
     rowForm.appendChild(row);
 }
 
+function messages(icon, title, button, timer){
+    Swal.fire({
+        //position: 'center',
+        type: icon,
+        title: title,
+        showConfirmButton: button,
+        timer: timer
+      })
+
+}
+
 $(document).ready(function() {
     $('#selectSupplier').select2();
     $('#selectProducts').select2();
 });
 
-addRow();
+//addRow();
 
 function clickPlus(e){
     //e.preventDefault();
     addRow();
+}
+
+function add(){
+    let quantity = inputQuantity.value;
+    let vlrUnit = inputVlrUnit.value;
+    let VlrTotal = inputVlrTotal.value;
+    let productText = selectProducts.options[selectProducts.selectedIndex].text;
+    let product = selectProducts.value;
+    if(quantity !== '' && vlrUnit !== '' && VlrTotal !== '' && product > 0){
+        itemsTotal++;
+        itemsView++;
+
+        formProductsList.append(`totalItems`, itemsTotal);
+        formProductsList.append(`totalView`, itemsView);
+        formProductsList.append(`item${itemsView}`, itemsTotal);
+        formProductsList.append(`product${itemsView}`, product);
+        formProductsList.append(`cant${itemsView}`, quantity);
+        formProductsList.append(`vlrUnit${itemsView}`, vlrUnit);
+
+        addRow(quantity, vlrUnit, VlrTotal, itemsView, productText, itemsTotal);
+        formProducts.reset();
+        $('#selectProducts').val(null).trigger('change');
+
+
+        const values = Object.fromEntries(formProductsList.entries());
+        console.log(values);
+    }
+}
+
+function rowRemove(item){
+    formProductsList.delete(`item${item}`);
+    formProductsList.delete(`product${item}`);
+    formProductsList.delete(`cant${item}`);
+    formProductsList.delete(`vlrUnit${item}`);
+    itemsTotal--;
+    formProductsList.append('totalItems', itemsTotal)
+
+    let row = document.getElementById(`row${item}`);
+    rowForm.removeChild(row);
+
+    let val = 1;
+    for(i = 0; i<itemsView; i++){
+        let input = document.getElementById(`number${i+1}`);
+        if(input !== null){
+            input.value = val;
+            val++;
+        }
+    }
+
+    const values = Object.fromEntries(formProductsList.entries());
+        console.log(values);
+}
+
+function send(){
+    let formSupplier = document.querySelector('#formSupplier');
+    let dataSupllier = new FormData(formSupplier);
+    for (const [nombre, valor] of dataSupllier.entries()) {
+        formProductsList.append(nombre, valor);
+      }
+      const values = Object.fromEntries(formProductsList.entries());
+      if(values.date == '' || values.numberInvoice == '' || values.supplier < 1){
+        return messages('error', 'Verifique Proveedor, numero de factura y fecha', true)
+      }
+      console.log('despues de if');
+      console.log(values);
 }
--- resources/views/admin/shopping_invoices.blade.php
+++ resources/views/admin/shopping_invoices.blade.php
@@ -12,12 +12,12 @@
 @section('content')
     <div class="container-fluid containers">
         <h1>Compras</h1>
-        <form >
-            <div class="col-md-8">
+        <div class="col-md-10">
+            <form id="formSupplier">
                 <div class="form-row">
                     <div class="form-group col-md-4">
                         <label for="costo">Fecha factura</label>
-                        <input type="date" class="form-control" name="date" id="inputDate"/>
+                        <input type="date" class="form-control" name="date" id="inputDate" />
                     </div>
                     <div class="col-md-4"></div>
                     <div class="form-group col-md-4">
@@ -28,68 +28,71 @@
                 <div class="form-row">
                     <div class="form-group col-md-8">
                         <label for="proveedor">Proveedor</label>
-                        <select name="supplier" id="selectSupplier" class="form-control" >
+                        <select name="supplier" id="selectSupplier" class="form-control">
                             <option value="-1">Seleccione Proveedor</option>
-                            @foreach($suppliers as $supplier)
-                            <option value="{{ $supplier->id }}">{{$supplier->dni}} - {{$supplier->name}}</option>
+                            @foreach ($suppliers as $supplier)
+                                <option value="{{ $supplier->id }}">{{ $supplier->dni }} - {{ $supplier->name }}</option>
                             @endforeach
                         </select>
                     </div>
                 </div>
-
-                <div class="container bg-white">
-                    <label for="">Agregar Productos</label>
-                    <div class="form-row">
-                        <div class="form-group col-md-7">
-                            <label for="proveedor">Producto</label>
-                            <select name="product" id="selectProducts" class="js-example-theme-single form-control" >
-                                <option value="-1">Seleccione Producto</option>
-                                @foreach($products as $product)
-                                <option value="{{ $product->id }}">{{$product->code}} - {{$product->name}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-md-1"></div>
-                        <div class="form-group col-md-2">
-                            <label for="quantity">Cantidad</label>
-                            <input type="number" class="form-control" id="inputQuanity" >
-                        </div>
+            </form>
+            <div class="container bg-white">
+                <label for="">Agregar Productos</label>
+                <form id="formProducts">
+                <div class="form-row">
+                    <div class="form-group col-md-7">
+                        <label for="proveedor">Producto</label>
+                        <select name="product" id="selectProducts" class="js-example-theme-single form-control">
+                            <option value="-1">Seleccione Producto</option>
+                            @foreach ($products as $product)
+                                <option value="{{ $product->id }}">{{ $product->code }} - {{ $product->name }}</option>
+                            @endforeach
+                        </select>
                     </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-3">
-                            <label for="vlr-Unitario" >Vlr. Unitario</label>
-                            <input type="number" class="form-control" id="inputVlrUnitario" placeholder="$ 0">
-                        </div>
-                        <div class="form-group col-md-3">
-                            <label for="vlr-Total" >Vlr. Total</label>
-                            <input type="number" class="form-control" id="inputVlrTotal" placeholder="$ 0">
-                        </div>
-                    </div>
-                    <div class="form-row justify-content-center pb-4 mb-4">
-                        <button class="btn btn-success">Agregar</button>
+                    <div class="col-md-1"></div>
+                    <div class="form-group col-md-2">
+                        <label for="quantity">Cantidad</label>
+                        <input type="number" class="form-control" id="inputQuantity" onchange="changeVlrUnit()">
                     </div>
                 </div>
-
-                <div class="border border-primary p-1 pl-2">
-                    <div class="form-row">
-                        <div class="col-md-1 border border-dark">
-                            <label for="">No.</label>
-                        </div>
-                        <div class="col-md-4 border border-dark">
-                            <label for="">Producto</label>
-                        </div>
-                        <div class="col-md-2 border border-dark">
-                            <label for="">Cant</label>
-                        </div>
-                        <div class="col-md-2 border border-dark">
-                            <label for="">Vlr.Unit</label>
-                        </div>
-                        <div class="col-md-2 border border-dark">
-                            <label for="">Total</label>
-                        </div>
+                <div class="form-row">
+                    <div class="form-group col-md-3">
+                        <label for="vlr-Unitario">Vlr. Unitario</label>
+                        <input type="number" class="form-control" id="inputVlrUnitario" placeholder="$ 0"
+                            onchange="changeVlrUnit()">
                     </div>
-                    <div id="rowForm">
-                        {{-- <div class="form-row">
+                    <div class="form-group col-md-3">
+                        <label for="vlr-Total">Vlr. Total</label>
+                        <input type="number" class="form-control" id="inputVlrTotal" placeholder="$ 0" tabindex="-1" disabled>
+                    </div>
+                </div>
+                <div class="form-row justify-content-center pb-4 mb-4">
+                    <button class="btn btn-info" type="button" onclick="add()">Agregar</button>
+                </div>
+                </form>
+            </div>
+
+            <div class="border border-primary p-1 pl-2">
+                <div class="form-row">
+                    <div class="col-md-1 border border-dark">
+                        <label for="">No.</label>
+                    </div>
+                    <div class="col-md-5 border border-dark">
+                        <label for="">Producto</label>
+                    </div>
+                    <div class="col-md-1 border border-dark">
+                        <label for="">Cant</label>
+                    </div>
+                    <div class="col-md-2 border border-dark">
+                        <label for="">Vlr.Unit</label>
+                    </div>
+                    <div class="col-md-2 border border-dark">
+                        <label for="">Total</label>
+                    </div>
+                </div>
+                <div id="rowForm">
+                    {{-- <div class="form-row">
                             <div class="col-md-1 border border-dark p-0">
                                 <input type="text" value="10"  class="form-control col-md-12">
                             </div>
@@ -106,13 +109,16 @@
                                 <input type="text" value="20000"  class="form-control col-md-12">
                             </div>
                         </div> --}}
-                    </div>
                 </div>
             </div>
-        </form>
+            <div class="form-row justify-content-center m-3">
+                <button class="btn btn-success" onclick="send()">Guardar Compra</button>
+            </div>
+        </div>
     </div>
 @stop
 @section('footer')
+
 @stop
 
 @section('plugins.Select2', true)
