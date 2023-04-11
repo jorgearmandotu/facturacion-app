c84d65489d4b52fdf17c48ab1efe03b9131ad5f0
armando <jorgearmandou@gmail.com>
2023-04-10 23:07:51 -0500

    se crea archivo de rutas jetstream para reemplazar el archivo que viene de fabrica

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,15 @@ Route::get('/', function () {
     return redirect()->route('login');
 });
 
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified'
-// ])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-//     //Route::get('/home', [HomeController::class, 'index'])->name('home');
-// });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+    //Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
+
+require_once __DIR__ . '/jetstream.php';
